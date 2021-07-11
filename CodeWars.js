3d7a2a181c2e52082134a42c4f4d9745be0cb90b
Talia Pichardo <taliapichardo@gmail.com>
2021-07-11 17:38:32 -0400

    NeedleInHaystack challenge

--- CodeWars.js
+++ CodeWars.js
@@ -8,8 +8,32 @@
 // `Hello, World!`
 
 const helloWorld = () => {
-    console.log(String.fromCharCode(72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33))
     return String.fromCharCode(72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33)
 }
 
-helloWorld()
+console.log(helloWorld())
+
+// Can you find the needle in the haystack ?
+
+// Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+// After your function finds the needle it should return a message(as a string) that says:
+
+// "found the needle at position " plus the index it found the needle, so:
+
+// findNeedle(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk'])
+// should return "found the needle at position 5"
+
+function findNeedle(haystack) {
+    //Initial approach:
+    //   for(let i = 0; i < haystack.length; i++){
+    //     if(haystack[i] === "needle"){
+    //       return `found the needle at position ${i}` 
+    //     }
+    //   }
+
+    const index = haystack.findIndex(word => word === "needle");
+    return `found the needle at position ${index}`
+}
+
+console.log(findNeedle(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk']))
