ed1f6ecc47d4c1dc1aa463b9e59c63f546818d1b
Talia Pichardo <taliapichardo@gmail.com>
2021-07-12 20:33:00 -0400

    Adds even_or_odd Challenge

--- CodeWars.js
+++ CodeWars.js
@@ -79,3 +79,17 @@ function countSheeps(arrayOfSheep) {
 }
 
 console.log(countSheeps([true, true, true, false, true, true, true, true]))
+
+// Create a function (or write a script in Shell) that takes an integer as an argument and returns "Even" for even numbers or 
+//"Odd" for odd numbers.
+
+function even_or_odd(number) {
+    if (number % 2 === 0) {
+        return 'Even'
+    } else {
+        return 'Odd'
+    }
+
+}
+
+console.log(even_or_odd(3))
