73d245987b9fb3b2b5036b243f1c6d301a7672a6
Talia Pichardo <taliapichardo@gmail.com>
2021-07-15 10:27:57 -0400

    Adds description for Vowels challenge

--- hackerRank.js
+++ hackerRank.js
@@ -1,3 +1,5 @@
+// 1. print each vowel in on a new line.The English vowels are a, e, i, o, and u, and each vowel must be printed in the same order as it appeared in .
+// 2. print each consonant(i.e., non - vowel) in on a new line in the same order as it appeared in
 
 function vowelsAndConsonants(s) {
     let vowels = 'aeiou';
