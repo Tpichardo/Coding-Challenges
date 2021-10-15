a63dbe2b144b208af5de56999496d4ff67396773
Talia Pichardo <taliapichardo@gmail.com>
2021-10-14 20:42:25 -0400

    Adds getEvenNumbers challenge

--- CodeWars.js
+++ CodeWars.js
@@ -202,3 +202,18 @@ function createPhoneNumber(numbers) {
 }
 
 console.log(createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]))
+
+
+// JavaScript Arrays support a filter function (starting in JavaScript 1.6). Use the filter functionality to complete the function given.
+
+// The solution would work like the following:
+
+// getEvenNumbers([2, 4, 5, 6]) 
+// should == [2,4,6]
+
+function getEvenNumbers(numbersArray) {
+    // filter out the odd numbers
+    return numbersArray.filter(num => num % 2 === 0)
+}
+
+console.log(getEvenNumbers([1, 2, 3, 4, 5, 6, 7]))
