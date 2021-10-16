6579b588e5483b350231ff0be3300879de9641d3
Talia Pichardo <taliapichardo@gmail.com>
2021-10-15 21:06:40 -0400

    Adds findMagic challenge

--- CodeWars.js
+++ CodeWars.js
@@ -258,3 +258,19 @@ function feast(beast, dish) {
 
 console.log(feast("great blue heron", "garlic naan"))
 console.log(feast("brown bear", "bear claw"))
+
+
+/*A magic index in an array A[1...n - 1] is defined to be an index such that A[i] = i.
+Given a sorted array of distinct integers, write a method to find a magic index, if one exists, in array A.*/
+
+function findMagic(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (i === arr[i]) {
+            return i
+        }
+    }
+    return -1
+}
+
+console.log(findMagic([10, 20, 2, 8]))
+console.log(findMagic([0, 4, 8, 10, 7]))
