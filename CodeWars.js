96001ea7446864b3105882121523f93a10fab18e
Talia Pichardo <taliapichardo@gmail.com>
2021-10-15 21:12:16 -0400

    Adds iqTest challenge

--- CodeWars.js
+++ CodeWars.js
@@ -274,3 +274,40 @@ function findMagic(arr) {
 
 console.log(findMagic([10, 20, 2, 8]))
 console.log(findMagic([0, 4, 8, 10, 7]))
+
+
+/*Bob is preparing to pass IQ test.The most frequent task in this test is 
+to find out which one of the given numbers differs from the others.
+Bob observed that one number usually differs from the others in evenness.
+Help Bob — to check his answers, he needs a program that among the given numbers finds one that is different
+ in evenness, and return a position of this number.*/
+
+/*!Keep in mind that your task is to help Bob solve a real IQ test, 
+which means indexes of the elements start from 1(not 0)*/
+
+// Examples:
+// iqTest("2 4 7 8 10") => 3 
+// Third number is odd, while the rest of the numbers are even
+
+// iqTest("1 2 1 1") => 2
+// Second number is even, while the rest of the numbers are odd
+
+function iqTest(numbers) {
+    let odd = ''
+    let even = ''
+    let arr = numbers.split(' ')
+    arr.unshift("")
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 === 0) {
+            even += i
+        } else {
+            odd += i
+        }
+    }
+
+    return Math.min(odd, even)
+}
+
+console.log(iqTest("2 4 7 8 10"))
+console.log(iqTest("1 2 2"))
