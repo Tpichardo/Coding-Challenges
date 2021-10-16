54d50dfb934875dcbf1eb4d4e5607dfc2a4b361a
Talia Pichardo <taliapichardo@gmail.com>
2021-10-16 17:53:55 -0400

    Adds well challenge

--- CodeWars.js
+++ CodeWars.js
@@ -374,3 +374,22 @@ function gooseFilter(birds) {
 };
 
 console.log(gooseFilter(["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]))
+
+
+/*In this kata you need to check the provided array(x) for good ideas 'good' and bad ideas 'bad'.
+If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'.
+If there are no good ideas, as is often the case, return 'Fail!'.*/
+
+
+function well(x) {
+    let goodIdeas = 0
+    if (!x.includes('good')) {
+        return 'Fail!'
+    }
+    for (let i = 0; i < x.length; i++) {
+        if (x[i] === 'good') {
+            goodIdeas += 1
+        }
+    }
+    return goodIdeas > 2 ? 'I smell a series!' : 'Publish!'
+}
