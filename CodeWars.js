6460692c65d11baea0a5537e59db0dd5f2daeeb8
Talia Pichardo <taliapichardo@gmail.com>
2021-10-17 18:51:28 -0400

    Adds XO challenge

--- CodeWars.js
+++ CodeWars.js
@@ -389,3 +389,24 @@ function well(x) {
 console.log(well(['good', 'good', 'good']))
 console.log(well(['bad', 'bad', 'good']))
 console.log(well(['bad', 'bad', 'bad']))
+
+
+/*Check to see if a string has the same amount of 'x's and 'o's.
+The method must return a boolean and be case insensitive.The string can contain any char.*/
+
+function XO(str) {
+    let x = 0
+    let o = 0
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i].toLowerCase() === 'x') {
+            x += 1
+        } else if (str[i].toLowerCase() === 'o') {
+            o += 1
+        }
+    }
+    return x === o ? true : false
+}
+
+console.log(XO("ooxXm"))
+console.log(XO("xooxx"))
