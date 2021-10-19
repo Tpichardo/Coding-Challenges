53554e75a4d1586b9359d0a56c719a8a420e4749
Talia Pichardo <taliapichardo@gmail.com>
2021-10-18 22:11:27 -0400

    Adds countPositives

--- CodeWars.js
+++ CodeWars.js
@@ -436,3 +436,29 @@ function twoSort(s) {
 
 console.log(twoSort('bitcoin'))
 console.log(twoSort('hello'))
+
+
+
+/*Given an array of integers.
+
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers.
+
+If the input array is empty or null, return an empty array.*/
+
+function countPositivesSumNegatives(input) {
+    if (input === null || input.length === 0) {
+        return []
+    }
+    let newArr = [0, 0]
+    for (let i = 0; i < input.length; i++) {
+        if (input[i] > 0) {
+            newArr[0] += 1
+        } else {
+            newArr[1] += input[i]
+        }
+    }
+    return newArr;
+}
+
+console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]))
+
