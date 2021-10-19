0b64547f61ae0683f9fe95be033d3a43c77d8fc5
Talia Pichardo <taliapichardo@gmail.com>
2021-10-18 22:14:27 -0400

    Adds splitSentence challenge

--- CodeWars.js
+++ CodeWars.js
@@ -462,3 +462,14 @@ function countPositivesSumNegatives(input) {
 
 console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]))
 
+
+
+/*Write function splitSentence which will create a list of strings from a string.
+Example:
+
+"hello world" -> ["hello", "world"]*/
+
+function splitSentence(s) {
+    return s.split(' ');
+}
+
