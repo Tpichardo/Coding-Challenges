9a0905c408617ef3d51200c26bf49b9a9714edaf
Talia Pichardo <taliapichardo@gmail.com>
2021-10-20 21:11:57 -0400

    Adds likes challenge

--- CodeWars.js
+++ CodeWars.js
@@ -547,3 +547,39 @@ function highestRank(arr) {
 }
 
 console.log(highestRank([12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10]))
+
+
+
+/*Implement the function which takes an array containing the names of people that like an item.
+It must return the display text as shown in the examples:*/
+
+/*[]-- > "no one likes this"
+["Peter"]-- > "Peter likes this"
+["Jacob", "Alex"]-- > "Jacob and Alex like this"
+["Max", "John", "Mark"]-- > "Max, John and Mark like this"
+["Alex", "Jacob", "Mark", "Max"]-- > "Alex, Jacob and 2 others like this"*/
+
+
+function likes(names) {
+    if (names.length === 0) {
+        return 'no one likes this'
+    } if (names.length === 1) {
+        return `${names[0]} likes this`
+    } else if (names.length === 2) {
+        return `${names[0]} and ${names[1]} like this`
+    } else if (names.length === 3) {
+        return `${names[0]}, ${names[1]} and ${names[2]} like this`
+    } else if (names.length === 4) {
+        return `${names[0]}, ${names[1]} and 2 others like this`
+    } else {
+        return `${names[0]}, ${names[1]} and` + ' ' + Number(names.length - 2) + ' ' + `others like this`
+    }
+
+}
+
+console.log(likes([]))
+console.log(likes(['Alex']))
+console.log(likes(['Alex', 'Jacob']))
+console.log(likes(['Alex', 'Jacob', 'Mark']))
+console.log(likes(['Alex', 'Jacob', 'Mark', 'Max']))
+console.log(likes(['Alex', 'Jacob', 'Mark', 'Max', 'Luis', 'John']))
