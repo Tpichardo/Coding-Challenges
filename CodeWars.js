535410d31461cfb648222b8666bcea1a50e25b0d
Talia Pichardo <taliapichardo@gmail.com>
2021-10-21 19:21:10 -0400

    Adds solution challenge

--- CodeWars.js
+++ CodeWars.js
@@ -614,3 +614,13 @@ function sumMix(x) {
 }
 
 console.log(sumMix(['2', 3, 6, '7', '10', 10]))
+
+
+/*Finish the solution so that it sorts the passed in array of numbers.
+If the function passes in an empty array or null value then it should return an empty array.*/
+
+function solution(nums) {
+    return nums === null || nums.length === 0 ? [] : nums.sort((a, b) => a - b)
+}
+
+console.log(solution([1, 2, 10, 50, 5]))
