9f84b8478210867f453b2a1a1b492bf04127c9c4
Talia Pichardo <taliapichardo@gmail.com>
2021-10-22 23:33:38 -0400

    Adds noSpace challenge

--- CodeWars.js
+++ CodeWars.js
@@ -671,3 +671,10 @@ function repeatStr(n, s) {
 }
 
 console.log(repeatStr(7, 'I'))
+
+// Simple, remove the spaces from the string, then return the resultant string.
+function noSpace(x) {
+    return x.split(' ').join('')
+}
+
+console.log(noSpace('8 j 8   mBliB8g  imjB8B8  jl  B'))
