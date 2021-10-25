8739fd572338234b0eb3665aaaca009491b0875c
Talia Pichardo <taliapichardo@gmail.com>
2021-10-24 22:33:43 -0400

    Adds hoopCount challenge

--- CodeWars.js
+++ CodeWars.js
@@ -688,3 +688,16 @@ function squareSum(numbers) {
 }
 
 console.log(squareSum([0, 3, 4, 5]))
+
+
+
+// Write a program where Alex can input(n) how many times the hoop goes round and it will return him an encouraging message
+
+// If Alex gets 10 or more hoops, return the string "Great, now move on to tricks".
+// If he doesn't get 10 hoops, return the string "Keep at it until you get it".
+function hoopCount(n) {
+    return n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it"
+}
+
+console.log(hoopCount(11))
+console.log(hoopCount(3))
