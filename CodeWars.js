3284976136079ba89ec342d88f44b5cf30faf22e
Talia Pichardo <taliapichardo@gmail.com>
2021-11-05 23:35:26 -0400

    Adds findAverage challenge

--- CodeWars.js
+++ CodeWars.js
@@ -1041,3 +1041,16 @@ var countSheep = function (num) {
 
 console.log(countSheep(5))
 
+// Find the mean(average) of a list of numbers in an array.
+
+
+var findAverage = function (nums) {
+    // Code here
+    let sum = 0
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i]
+    }
+    return sum / nums.length
+}
+
+console.log(findAverage([1, 3, 5, 7]))
