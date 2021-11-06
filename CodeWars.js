095edd6820cc5766417c7018cb0ecbc697432941
Talia Pichardo <taliapichardo@gmail.com>
2021-11-05 23:39:42 -0400

    Adds getSize Challenege

--- CodeWars.js
+++ CodeWars.js
@@ -1073,3 +1073,10 @@ function areYouPlayingBanjo(name) {
 
 console.log(areYouPlayingBanjo('Adam'))
 console.log(areYouPlayingBanjo('Raul'))
+
+// Write a function that returns the total surface area and volume of a box as an array: [area, volume]
+function getSize(width, height, depth) {
+    return [(2 * depth * width + 2 * depth * height + 2 * width * height), (width * height * depth)];
+}
+
+console.log(getSize(4, 2, 6))
