e2e337f6f5cfc286fded3ff2e598c6a1867db31f
Talia Pichardo <taliapichardo@gmail.com>
2021-11-05 23:42:46 -0400

    Adds digitize challenge

--- CodeWars.js
+++ CodeWars.js
@@ -1080,3 +1080,9 @@ function getSize(width, height, depth) {
 }
 
 console.log(getSize(4, 2, 6))
+
+// Given a random non - negative number, you have to return the digits of this number within an array in reverse order.
+function digitize(n) {
+    return n.toString().split('').map(str => Number(str)).reverse()
+}
+console.log(digitize([795843]))
