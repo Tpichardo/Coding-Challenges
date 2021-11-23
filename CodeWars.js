c5bc65ef659bdd89e5ee840a32f274bf60167aee
Talia Pichardo <taliapichardo@gmail.com>
2021-11-22 23:16:50 -0500

    Adds explode challenge

--- CodeWars.js
+++ CodeWars.js
@@ -1299,3 +1299,15 @@ function dontGiveMeFive(start, end) {
 
 console.log(dontGiveMeFive(4, 17))
 console.log(dontGiveMeFive(1, 9))
+
+//Given a string made of digits[0 - 9], return a string where each digit is repeated a number of times equals to its value.
+function explode(s) {
+    let newStr = '';
+    for (let i = 0; i < s.length; i++) {
+        newStr += s[i].repeat(s[i])
+    }
+    return newStr;
+}
+
+console.log(explode("312"))
+console.log(explode("102269"))
