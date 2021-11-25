eff518c8caeb086f737bdb9d2e871c11f4b527fe
Talia Pichardo <taliapichardo@gmail.com>
2021-11-24 23:34:48 -0500

    Adds smallEnough challenge

--- CodeWars.js
+++ CodeWars.js
@@ -1343,3 +1343,15 @@ function divisibleByLast(n) {
 
 console.log(divisibleByLast(73312))
 console.log(divisibleByLast(2026))
+
+
+/*You will be given an array and a limit value.You must check that all values in the array are below or equal to the limit value.
+If they are, return true.Else, return false.*/
+
+function smallEnough(a, limit) {
+    return a.every(x => x <= limit)
+}
+
+console.log(smallEnough([66, 101], 200))
+console.log(smallEnough([78, 117, 110, 99, 104, 117, 107, 115], 100))
+console.log(smallEnough([101, 45, 75, 105, 99, 107], 107))
