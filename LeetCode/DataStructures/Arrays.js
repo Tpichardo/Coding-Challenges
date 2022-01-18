7ccd750a57f4c0bdf32c68bc93f19c9cf48661e8
Talia Pichardo <taliapichardo@gmail.com>
2022-01-17 23:27:21 -0500

    Adds twoSum Solution

---
+++ LeetCode/DataStructures/Arrays.js
@@ -0,0 +1,16 @@
+const twoSum = (nums, target) => {
+    let map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        let num1 = nums[i];
+        let num2 = target - nums[i];
+        if (map.has(num2)) {
+            return [map.get(num2), i]
+        }
+        map.set(num1, i)
+    }
+
+}
+
+console.log(twoSum([2, 7, 11, 15], 9)); // => [0, 1]
+console.log(twoSum([3, 2, 4], 6)); // => [1, 2]
+console.log(twoSum([3, 3], 6)); // => [0, 1]
