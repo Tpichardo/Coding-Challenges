b394ed587808fcf399d4f46265a079fa3eeb339e
Talia Pichardo <taliapichardo@gmail.com>
2022-02-01 14:59:17 -0500

    Adds array solution for twoSum

--- LeetCode/DataStructures/Arrays.js
+++ LeetCode/DataStructures/Arrays.js
@@ -1,12 +1,10 @@
 const twoSum = (nums, target) => {
-    let map = new Map();
     for (let i = 0; i < nums.length; i++) {
-        let num1 = nums[i];
-        let num2 = target - nums[i];
-        if (map.has(num2)) {
-            return [map.get(num2), i]
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j]
+            }
         }
-        map.set(num1, i)
     }
 
 }
